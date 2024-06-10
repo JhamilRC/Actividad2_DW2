9ba432cb1915f89d4b2e5d68156dcf63e3b6ae3b
JhamilRc <jhamilrocha11@gmail.com>
2024-06-10 12:26:19 -0400

    practica

---
+++ 03BaseNode/App.js
@@ -0,0 +1,8 @@
+const colors = require('colors');
+//console.log('=========================='.red)
+//console.log('     tabla del '.red,colors.blue(num));
+//console.log('=========================='.red)
+const num=5;
+for (let i=1; i<=10; i++){
+    console.log(`${num} x ${i} = =${num*i}`.green);
+}
